151ffd438ee75b96fed8d8dcd9e9f352883254b4
rkudhaya10 <86477873+rkudhaya10@users.noreply.github.com>
2021-06-26 17:18:39 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java script and React.Js</h1>
+      <p>ShapeAI Bootcamp</p>
+      <h1>ShapeAI Bootcamp 2021</h1>
+      <p>Shaurya sinha</p>
+    </div>
+  );
+}
+
+export default Info;
